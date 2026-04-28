c9539c19de900dc207982f411ca4f90e75b377e6
Damilola Ajele <damilolaajele05@gmail.com>
2026-04-28 02:52:59 +0100

    Updating Server

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "birthday-reminder-app-ashen.vercel.app",
+    origin: "*",
     credentials: true,
     Methods: ["POST"],
   }),
